bc81b8ab1b472b11d594e1cc07297f1554eb42b6
Michael Salvatore <msalvatore22@gmail.com>
2023-02-14 16:45:41 -0500

    Connected flask app to atlas mongodb cluster

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+import db
+import json
+from bson import json_util
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+@app.route('/')
+def flask_mongodb_atlas():
+    return "flask mongodb atlas!"
+
+
+#test to insert data to the data base
+@app.route("/test")
+def test():
+    findOne = db.collection.find_one()
+    return parse_json(findOne)
+
+if __name__ == '__main__':
+    app.run(port=5000)
